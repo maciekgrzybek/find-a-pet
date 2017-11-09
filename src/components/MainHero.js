e5f801cbd290d56fe02caba29276fcbe90ad3f49
Maciek Grzybek <maciek@dsemotion.com>
2017-11-09 14:09:03 +0000

    working

--- src/components/MainHero.js
+++ src/components/MainHero.js
@@ -3,7 +3,12 @@ import React from 'react';
 const MainHero = () => {
 	return (
 		<div className="hero">		
-			<h1>Zgubiles?</h1>
+			<div className="container">
+				<div className="row">
+					<div className="col-xs-6"></div>
+					<div className="col-xs-6">Siemanko</div>
+				</div>
+			</div>
 		</div>		
 	);
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,29 +6,31 @@ import Icon from './Icon';
 const Navigation = () => {
 	return (
 
-			<div className="grid top-bar middle-xs">
-				<div className="col-6_xs-4_sm-6_md-4_lg-2 top-bar__logo">
-					<NavLink to="/">
-						<Icon 
-							icon="logo"
-							width="100%"
-							height="40px" />
-					</NavLink>
-				</div>
-				<div className="col-6_xs-8_sm-6_md-4_lg-2 top-bar__menu">
-
-						<ul className="navigation end-xs">
-							<li className="navigation__item">								
-								<NavLink exact to="/"><Icon icon="home" /><span>Strona główna</span></NavLink>
-							</li>
-							<li className="navigation__item">								
-								<NavLink exact to="/rodzaj-ogloszenia"><Icon icon="advert" /><span>Dodaj ogłoszenie</span></NavLink>
-							</li>
-							<li className="navigation__item">								
-								<NavLink exact to="/kontakt"><Icon icon="contact" /><span>Kontakt</span></NavLink>
-							</li>
-						</ul>
-
+			<div className="container">
+				<div className="row top-bar">
+					<div className="col-xs-4 col-sm-8 col-md-6 col-lg-4 top-bar__logo">
+						<NavLink to="/">
+							<Icon 
+								icon="logo"
+								width="100%"
+								height="40px" />
+						</NavLink>
+					</div>
+					<div className="col-xs-8 col-sm-8 col-md-6 col-lg-4 top-bar__menu">
+	
+							<ul className="navigation end-xs">
+								<li className="navigation__item">								
+									<NavLink exact to="/"><Icon icon="home" /><span>Strona główna</span></NavLink>
+								</li>
+								<li className="navigation__item">								
+									<NavLink exact to="/rodzaj-ogloszenia"><Icon icon="advert" /><span>Dodaj ogłoszenie</span></NavLink>
+								</li>
+								<li className="navigation__item">								
+									<NavLink exact to="/kontakt"><Icon icon="contact" /><span>Kontakt</span></NavLink>
+								</li>
+							</ul>
+	
+					</div>
 				</div>
 			</div>
 	);
