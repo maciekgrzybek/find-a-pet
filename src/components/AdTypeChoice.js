21f9acd29fe7c05b05a5fa5cb7051fed775d0dc0
maciek_grzybek <maciekgrzybek1@gmail.com>
2018-01-18 08:01:55 +0000

    Styling ...

--- src/components/AdTypeChoice.js
+++ src/components/AdTypeChoice.js
@@ -5,7 +5,9 @@ const AdTypeChoice = props => {
   return (
     <div className="small-12 large-4 cell">
       <Link to={props.path}>
-        <div className={`ads-type__choice ads-type__choice--${props.adType}`}>{props.label}</div>
+        <div className={`ads-type__choice ads-type__choice--${props.adType}`}>
+          <p>{props.label}</p>
+        </div>
       </Link>
     </div>
   );
